adcfc74f0b4d8772211838371eec3c1b2a2caf04
totty <kaqiu96@gmail.com>
2024-03-01 19:41:50 +0100

    first step okay

---
+++ src/main/java/com/cacttus/locationservice/configs/FlyWayMigration.java
@@ -0,0 +1,16 @@
+package com.cacttus.locationservice.configs;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlyWayMigration {
+    @Bean
+    public FlywayMigrationStrategy runMigration() {
+        return flyway -> {
+            flyway.migrate();
+            flyway.repair();
+        };
+    }
+}
